c27534f0fb4aeec73c6a405e83746bb864ee1852
Goran <tenic.g@gmail.com>
2021-03-12 21:46:22 +0100

    added nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import { useRef } from "react";
+import NProgress from "nprogress";
+import Router from "next/router";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { Hydrate } from "react-query/hydration";
 import PropTypes from "prop-types";
 import Layout from "../components/layout/Layout";
 import "../styles/globals.css";
+
+import "nprogress/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 // eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
